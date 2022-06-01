8c19bc371e87e93c6a5c3ba6205882160b25e360
Rakib0101 <rakib.01jan@gmail.com>
2022-06-01 08:27:52 +0600

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use App\Models\Doctor;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Admin::create([
+            'name' => 'admin',
+            'phone' => '01644229013',
+            'email' => 'admin@mail.com',
+            'email_verified_at' => null,
+            'password' => Hash::make('111111')
+        ]);
+        Doctor::create([
+            'name' => 'doctor',
+            'hospital' => 'HpB',
+            'email' => 'doctor@mail.com',
+            'email_verified_at' => null,
+            'password' => Hash::make('111111')
+        ]);
+        User::create([
+            'name' => 'user',
+            'email' => 'user@mail.com',
+            'email_verified_at' => null,
+            'password' => Hash::make('111111')
+        ]);
+    }
+}
